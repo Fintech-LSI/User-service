a45b75cda587dfb903cd685e43223385ad766c91
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2025-01-12 14:27:02 +0100

    add incom

--- src/main/java/com/fintech/user/controller/dto/responses/UserResponse.java
+++ src/main/java/com/fintech/user/controller/dto/responses/UserResponse.java
@@ -1,24 +1,28 @@
 package com.fintech.user.controller.dto.responses;
 
+import com.fintech.user.entity.OwnerShip;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import org.antlr.v4.runtime.misc.NotNull;
 
 public record UserResponse(
-  Long id,
-  @NotNull()
-  @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
-  String firstName,
+        Long id,
+        @NotNull()
+        @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
+        String firstName,
 
-  @NotNull()
-  @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
-  String lastName,
+        @NotNull()
+        @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
+        String lastName,
 
-  @NotNull()
-  @Email(message = "Email should be valid")
-  String email,
+        @NotNull()
+        @Email(message = "Email should be valid")
+        String email,
 
-  Integer age ,
+        Integer age ,
+        Double salary,
+        OwnerShip homeOwnership,
+        Integer employmentMonth,
 
-  String image
+        String image
 ) {}
