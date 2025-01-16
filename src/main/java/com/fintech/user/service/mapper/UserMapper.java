a1107deaaf5a713e32f90304c5b514aed3e271b6
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2025-01-16 20:47:43 +0100

    Update UserMapper.java

--- src/main/java/com/fintech/user/service/mapper/UserMapper.java
+++ src/main/java/com/fintech/user/service/mapper/UserMapper.java
@@ -8,13 +8,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserMapper {
   public User requestToUser(UserRequest userRequest) {
-     return User.builder()
-       .id(userRequest.id())
-       .email(userRequest.email())
-       .firstName(userRequest.firstName())
-       .lastName(userRequest.lastName())
-       .build();
-  }
+    return User.builder()
+        .id(userRequest.id())
+        .email(userRequest.email())
+        .firstName(userRequest.firstName())
+        .lastName(userRequest.lastName())
+        .age(userRequest.age())
+        .salary(userRequest.salary())
+        .homeOwnership(userRequest.homeOwnership())
+        .employmentMonth(userRequest.employmentMonth())
+        // Handle image separately as it might need to be processed
+        .build();
+}
 
 
   public UserResponse userToResponse(User user) {
